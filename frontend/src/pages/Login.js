9280462632ab49b2f42a4686a89535da67f53bd4
AYUSH KUMAR <yayush848@gmail.com>
2026-09-25 16:49:09 +0530

    Update login API URL to production endpoint

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -31,7 +31,7 @@ function Login() {
         }
 
         try{
-            const url = "http://localhost:8080/auth/Login";
+            const url = "https://auth-mern-app-api-pearl.vercel.app/";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
