c4a1e81e4adbcacb362a4f39b09e5bbaf288e512
AYUSH KUMAR <yayush848@gmail.com>
2026-09-25 16:51:32 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -31,7 +31,7 @@ function Signup() {
         }
 
         try{
-            const url = "http://localhost:8080/auth/signup";
+            const url = "https://auth-mern-app-api-pearl.vercel.app/auth/signup";
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
